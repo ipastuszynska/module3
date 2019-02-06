436b4aa03ef1f9da10309a230ad9245e99e426aa
iza pastuszynska <ipastuszynska@gmail.com>
2019-02-06 16:29:36 +0000

    added files for Ch08 and ch09

---
+++ ch09_PM_notes.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  5 14:46:23 2019
+
+@author: iza
+"""
+
+###############################################
+# CHAPTER 9 - DEFINING A SOFTWARE PROJECT
+##############################################
+MoSCoW Requirements Prioritization Technique
+Project : Phonebook
+MUST (M)
+- search for people and businesses in the UK.
+- for each person we must return: Name, Surname, Address, city, country, postcode, phone number
+- for each business we must return: Business name, Business type, address, city, country, postcode, phone number
+- calculate the distance between the result and the user input postcode
+- search by business type or name and postcode
+- search by surname and postcode
+SHOULD (S)
+- return a list sorted by distance
+- return a list sorted by alphabetical order
+COULD (C)
+- search people and businesses by city
+- return just a certain amount of results
+- view related Search results
+WON’T or WOULD (W)
+- pick the geolocation of the user
+- monetisation (examples: advertise a business, host third parties adv)
+- FAQ (examples: how to add my details, how to remove my details)
+
+"""
+1 Ideation
+
+2 Requirments gathering - must have, should have etc -> look at resources avilable 
++ identify skills needed 
+
+3 Development 
+4 Testing
+5 Deployment
+6 
+
+
+Scope, time , budge traingle (quality inside)
+
+
+"""
